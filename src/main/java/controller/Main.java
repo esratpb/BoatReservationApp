63393141510fe7f6112919c1b09ee4a7d16862e9
esratpb <51519312+esratpb@users.noreply.github.com>
2021-06-02 17:28:33 +0200

    Update Main.java

--- src/main/java/controller/Main.java
+++ src/main/java/controller/Main.java
@@ -1,7 +1,6 @@
 package controller;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import model.Model;
+
 
 import java.io.IOException;
 import java.text.ParseException;
@@ -9,8 +8,6 @@ import java.text.ParseException;
 
 public class Main {
     public static void main(String[] args) throws IOException, ParseException {
-        Model model = new Model();
-        ObjectMapper mapper = new ObjectMapper();
 
         MainController controller = new MainController();
         controller.execute();
